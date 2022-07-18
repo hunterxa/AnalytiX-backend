464ea5b6fa4db68c67a2da57fa3ccd17f2646e0e
hunterallen <jhallen327@gmail.com>
2022-07-18 15:08:41 -0500

    Added saving organizations to database with members

--- src/main/java/com/hunterxa/AnalytiX/organization/Organization.java
+++ src/main/java/com/hunterxa/AnalytiX/organization/Organization.java
@@ -2,18 +2,27 @@ package com.hunterxa.AnalytiX.organization;
 
 import com.hunterxa.AnalytiX.privilegeduser.PrivilegedUser;
 import com.hunterxa.AnalytiX.user.User;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
-@Table(name = "organizations")
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unique_name_constraint",
+                        columnNames = "name"
+                )
+        },
+        name = "organizations"
+)
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString(exclude = "id")
+@Builder
 public class Organization {
 
     @Id
@@ -28,24 +37,35 @@ public class Organization {
     )
     private Long id;
     private String name;
+    @Transient
+    private User creator;
 
 
-//    @ManyToMany(cascade = CascadeType.ALL)
-//    @JoinTable(
-//            name = "organization_member_mapping",
-//            joinColumns = @JoinColumn(
-//                    name = "fk_organization_id",
-//                    referencedColumnName = "id"
-//            ),
-//            inverseJoinColumns = @JoinColumn(
-//                    name = "fk_user_id",
-//                    referencedColumnName = "id"
-//            )
-//    )
-//    private List<User> members;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "organization_member_mapping",
+            joinColumns = @JoinColumn(
+                    name = "fk_organization_id",
+                    referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "fk_user_id",
+                    referencedColumnName = "id"
+            )
+    )
+    private List<User> members;
+
 
 //    @ManyToMany(cascade = CascadeType.ALL)
 //    @JoinTable()
-//    private List<PrivilegedUser> privilegedUsers;
+//    private List<PrivilegedUser> privilegedMembers;
+
+    public void addMember(User user) {
+        members.add(user);
+    }
+
+    public void printMembersDev() {
+        for (User m : members) System.out.println(m.toStringDev());
+    }
 
 }
--- src/main/java/com/hunterxa/AnalytiX/organization/OrganizationService.java
+++ src/main/java/com/hunterxa/AnalytiX/organization/OrganizationService.java
@@ -1,15 +1,36 @@
 package com.hunterxa.AnalytiX.organization;
 
+import com.hunterxa.AnalytiX.user.User;
+import com.hunterxa.AnalytiX.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrganizationService {
 
     @Autowired
     private OrganizationRepository organizationRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public void addNewOrganization(Organization organization) {
+
+        Optional<User> user = userRepository.findByEmail(organization.getCreator().getEmail());
+
+        if (user.isEmpty()) throw new IllegalStateException("no user found with that email");
+
+        //If user is found in database, make the persisted user the creator and add them to the members list
+        organization.setCreator(user.get());
+        List<User> members = new ArrayList<>();
+        members.add(user.get());
+        organization.setMembers(members);
+
+        //TODO: organization.addPrivilegedMember(user);
+
         organizationRepository.save(organization);
     }
 }
